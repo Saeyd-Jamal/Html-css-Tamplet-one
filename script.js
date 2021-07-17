f3d664bc59ac1cfccfde04bd1831d7d1f8cc2151
Al-Saeyd J Al-Akhrsi <79516145+Saeyd-Jamal@users.noreply.github.com>
2021-07-18 02:52:46 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var body = document.getElementById("body"),
+    loading = document.getElementById("loading");
+
+
+window.onload = function loding() {
+  loading.style.display="none";
+  body.style.display="inline";
+}
